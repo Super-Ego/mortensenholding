b4864be054678bfcb24dfbcd1e441b6359b3e6a0
Christian Plovst <chp@superego.nu>
2022-07-06 10:05:39 +0200

    Medarbejdere

--- superego/template-parts/blocks/medarbejdere/medarbejdere.php
+++ superego/template-parts/blocks/medarbejdere/medarbejdere.php
@@ -18,43 +18,45 @@ $className = $blockName . '-block section';
 // Import standard block settings
 require(get_template_directory() . '/functions/block-settings.php');
 
+// Register Isotope JS (CDN)
+wp_enqueue_script('isotope-js', 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js', ['jquery'], null, false);
+// Register Isotope
+wp_enqueue_script('isotope-js-call', THEME . '/assets/scripts/isotope.js', array('jquery'), false, false);
 ?>
 
 <!-- Medarbejdere block -->
 <section id="<?= esc_attr($id); ?>" class="<?= esc_attr($className); ?>">
     <div class="container">
-        <div class="row">
+        <div class="isotope-grid row">
             <?
             // WP_Query arguments
             $args = array(
                 'post_type'      => 'medarbejdere',
                 'posts_per_page' => -1,
                 'order'          => 'ASC',
-                'orderby'        => 'menu_order',
+                'orderby'        => 'post_date',
             );
 
             // The Query
             $query = new WP_Query($args);
-            $posts = $query->posts;
+            $medarbejdere = $query->posts;
 
-            foreach ($posts as $post) :
-                $stilling = get_field('medarbejder_stilling', $post->ID) ?: 'Stilling';
-                $email = get_field('medarbejder_email', $post->ID) ?: 'din@email.dk';
-                $telefon = get_field('medarbejder_telefonnummer', $post->ID) ?: '12345678';
+            foreach ($medarbejdere as $medarbejder) :
+                $stilling = get_field('medarbejder_stilling', $medarbejder->ID) ?: 'Stilling';
+                $email = get_field('medarbejder_email', $medarbejder->ID) ?: 'din@email.dk';
+                $telefon = get_field('medarbejder_telefonnummer', $medarbejder->ID) ?: '12345678';
             ?>
 
-                <div class="col-md-6 medarbejder-column">
+                <div class="col-12 col-md-6 col-xl-4 grid-item medarbejder-column">
                     <div class="medarbejder">
                         <figure class="image-figure">
-                            <?= the_post_thumbnail('full'); ?>
+                            <?= get_the_post_thumbnail($medarbejder->ID, 'full'); ?>
                         </figure>
                         <div class="medarbejder__inner">
-                            <h3 class="medarbejder__navn"><?= get_the_title($post->ID); ?></h3>
+                            <h3 class="h5 medarbejder__navn"><?= get_the_title($medarbejder->ID); ?></h3>
                             <p class="medarbejder__stilling"><?= $stilling; ?></p>
-                            <div class="medarbejder__kontakt">
-                                <a href="tel:<?= $telefon; ?>"><?= $telefon; ?></a>
-                                <a href="mailto:<?= $email; ?>"><?= $email; ?></a>
-                            </div>
+                            <a class="medarbejder__kontakt" href="tel:<?= $telefon; ?>"><span>Tlf.:</span> <?= $telefon; ?></a><br>
+                            <a class="medarbejder__kontakt" href="mailto:<?= $email; ?>"><span>Mail:</span> <?= $email; ?></a>
                         </div>
                     </div>
                 </div>
